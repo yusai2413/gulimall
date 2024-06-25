16d58c43a35d1da54ec684b39e47da84519d1f07
saiyu <yusai2413@gmail.com>
2024-06-25 14:44:32 +0800

    prod and coupon debug success

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,38 @@
 package com.atguigu.gulimall.product;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
-    void contextLoads() {
-    }
+    public void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("华为");
 
-}
+//        brandEntity.setName("华为2");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功...");
+
+//        brandService.updateById(brandEntity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        list.forEach((item) -> {
+            System.out.println(item);
+        });
+
+        }
+
+    }
